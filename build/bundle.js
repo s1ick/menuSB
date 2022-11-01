992dd07252729017291e661a46f1315ef2e40bc6
Ivan Matveev <imatveev@biarum.com>
2022-11-02 01:42:30 +0300

    menuSB

--- build/bundle.js
+++ build/bundle.js
@@ -1,1 +1,18 @@
+const hamburger = document.querySelector('#btn');
+const menu = document.querySelector('#menu');
+const submenuBtn = document.querySelector('.btn-js');
+const submenuBtn2 = document.querySelector('.btn-js2');
+const wrapper = document.querySelector('.wrapper');
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('show');
+    wrapper.classList.toggle('darkmode');
+});
 
+submenuBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    e.currentTarget.parentElement.classList.toggle('show');
+});
+submenuBtn2.addEventListener('click', (e) => {
+    e.preventDefault();
+    e.currentTarget.parentElement.classList.toggle('show');
+});
